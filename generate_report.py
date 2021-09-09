996ef6b9c924634fc495d3f99d76e7bd5dbc0d86
Kenan Alizadeh <kanan.alizada@ibar.az>
2021-09-09 21:00:14 +0400

    Documentation for generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -8,57 +8,83 @@ import numpy as np
 sys.dont_write_bytecode = True
 
 def main(workdir, scriptdir):
+    # Normalize workdir
     workdir = os.path.normpath(workdir)
 
+    # Open and load raw report json file
     data = open('{dir}/../DerivedData/raw_report.json'.format(dir=workdir),'r')
     json_data = json.loads(data.read())
 
+    # Open and load config file
     config_data = open('{dir}/config.json'.format(dir=scriptdir))
     configs = json.loads(config_data.read())
 
+    # Flatten all files, remove workdir from path, remove functions
     all_files = flatten([x['files'] for x in json_data['targets']])
     for i in range(len(all_files)):
         all_files[i]['path'] = all_files[i]['path'].replace(workdir, '')
         all_files[i].pop('functions', None)
 
+    # Squad names listed in config file
     squad_names = [x['name'] for x in configs['squads']]
 
+    # Flat list of all squad files
     files = flatten([process_files_for_squad(all_files, configs, x) for x in squad_names])
 
+    # Check if files are empty
     if not files:
         print('\n\u26A0\uFE0F  Could not generate report.')
         return
 
-    save_reports(workdir, all_files, files)
+    # Generate and save coverage report
+    save_report(workdir, all_files, files)
 
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
 def process_files_for_squad(all_files, configs, squad_name):
+    # Filenames for specified squad
     selected_filenames = flatten([x['filenames'] for x in configs['squads'] if x['name'] == squad_name])
+
+    # Check if filenames are not empty in config file
     if not selected_filenames:
         print('\n\u26A0\uFE0F  Filenames for squad {} must be provided for coverage report.'.format(squad_name))
         return []
 
+    # Populate squad files
     files = []
     for file in all_files:
-        if any([x in file['path'] for x in selected_filenames]):
-            file['squad'] = squad_name
-            files.append(file)
+        # Stop iterating if files matching squad filenames are all found
+        if len(files) == len(selected_filenames):
+            print("THE CYCLE ENDS HERE")
+            break
 
-    filenames = [x for x in selected_filenames if any([x in file['path'] for file in files])]
+        # Check if any squad filename matches the file path
+        if any([x in file['path'] for x in selected_filenames]):
+            # Populate squad field
+            file['squad'] = squad_name
+            # Add file
+            files.append(file)
 
     print('\n======================================================================')
 
     print('\n\u2705 Done! Coverage report generated from {} out of {} files for {}.\n'.format(len(files), len(selected_filenames), squad_name))
 
+    # Filenames from processed squad files
+    filenames = [x for x in selected_filenames if any([x in file['path'] for file in files])]
+
+    # Make a list of squad files missing from the list of all files
     missing_files = list(set(selected_filenames) - set(filenames))
+
+    # Report missing files
     if missing_files:
         print('\u26A0\uFE0F  {count} File(s) not found:'.format(count=len(missing_files)))
         [print(' - {}'.format(x)) for x in missing_files]
 
+    # Total coverage for squad files
     squad_total_coverage = total_coverage(files)
 
+    # Populate squad coverage field for each file
     for i in range(len(files)):
         files[i]['squad_total_coverage'] = squad_total_coverage
 
@@ -74,37 +100,62 @@ def total_coverage(files):
 
     return covered_lines / executable_lines if executable_lines else 0
 
-def generate_report_for_squad_files(files):
+def dataframe_for_squad_files(files):
+    # Load dataframe
     df = pd.DataFrame.from_dict(files)
+
+    # Format Line Coverage column as percentage
     df['lineCoverage'] = pd.Series(["{0:.2f}%".format(val * 100) for val in df['lineCoverage']], index = df.index)
+
+    # Format Squad Coverage column as percentage
     df['squad_total_coverage'] = pd.Series(["{0:.2f}%".format(val * 100) for val in df['squad_total_coverage']], index = df.index)
+
+    # Set column titles
     df.columns = ["Lines Covered", "Line Coverage", "File path", "File name", "Executable Lines", "Squad", "Squad Coverage"]
+
+    # Rearrange columns
     df = df[["Squad", "Squad Coverage", "File name", "Line Coverage", "Lines Covered", "Executable Lines", "File path"]]
 
     return df
 
-def generate_report_for_undetermined_files(files):
+def dataframe_for_undetermined_files(files):
+    # Load dataframe
     df = pd.DataFrame.from_dict(files)
+    
+    # Format Line Coverage column as percentage
     df['lineCoverage'] = pd.Series(["{0:.2f}%".format(val * 100) for val in df['lineCoverage']], index = df.index)
 
+    # Set column titles
     df.columns = ["Lines Covered", "Line Coverage", "File path", "File name", "Executable Lines"]
+
+    # Rearrange columns
     df = df[["File name", "Line Coverage", "Lines Covered", "Executable Lines", "File path"]]
 
     return df
 
-def save_reports(workdir, all_files, files):
-    df1 = generate_report_for_squad_files(files)
+def save_report(workdir, all_files, files):
+    # Dataframe for squad files
+    df1 = dataframe_for_squad_files(files)
 
+    # Undetermined files
     undetermined_files = [x for x in all_files if x['path'] not in [x['path'] for x in files]]
 
-    df2 = generate_report_for_undetermined_files(undetermined_files)
+    # Dataframe for undetermined files
+    df2 = dataframe_for_undetermined_files(undetermined_files)
 
+    # Merge squad and undetermined files
     df = pd.concat([df1, df2], ignore_index=True)
+
+    # Fix indexing to start from 1
     df.index = np.arange(1, len(df)+1)
 
+    # Path for report output files
     report_path = os.path.normpath('{dir}/../CoverageReport/'.format(dir=workdir))
 
+    # Export as csv
     df.to_csv("{dir}/report.csv".format(dir=report_path), na_rep='N/A')
+
+    # Export as html
     df.to_html("{dir}/report.html".format(dir=report_path), na_rep='N/A')
 
     print("\n\u2139\uFE0F  Enter following command to view coverage report.")
