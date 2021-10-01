15d560ada18c4e04e2465767da94902397e178d0
Kenan Alizadeh <kanan.alizada@ibar.az>
2021-10-01 12:39:52 +0400

    working on subprocesses

--- xctest.py
+++ xctest.py
@@ -308,22 +308,22 @@ def run_tests():
     workspace_file = "{dir}/IBAMobileBank.xcworkspace".format(dir=project_dir)
     xcpretty_output = '{dir}/xcpretty_tests.html'.format(dir=xctest_logs_dir)
 
-    # Check tuist
-    # if os.path.exists('{}/Project.swift'.format(project_dir)):
-    #     print('- Generating project with Tuist...')
-    #     # tuist_generate = subprocess.Popen([
-    #     #     'tuist',
-    #     #     'generate',
-    #     #     '--path {}/Project.swift'.format(project_dir)
-    #     #     ])
-    #     tuist_generate = subprocess.Popen(
-    #         [
-    #         'tuist',
-    #         'generate'
-    #         ],
-    #         cwd=project_dir
-    #         )
-    #     tuist_generate.communicate()
+    Check tuist
+    if os.path.exists('{}/Project.swift'.format(project_dir)):
+        print('- Generating project with Tuist...')
+        # tuist_generate = subprocess.Popen([
+        #     'tuist',
+        #     'generate',
+        #     '--path {}/Project.swift'.format(project_dir)
+        #     ])
+        tuist_generate = subprocess.Popen(
+            [
+            'tuist',
+            'generate'
+            ],
+            cwd=project_dir
+            )
+        tuist_generate.communicate()
 
     print('- Running tests for ABB Mobile...')
 
