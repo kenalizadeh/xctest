130bd8cf5c2881c99bdd96322d58fbdf1024115b
Kenan Alizadeh <kanan.alizada@ibar.az>
2021-10-03 18:37:46 +0400

    added script for convenient packaging

--- package.sh
+++ package.sh
@@ -1,2 +1,11 @@
-pyinstaller ./xctest.py --onedir --clean --distpath ./packaged --workpath ./packaged/build --specpath ./packaged
+# Create virtual environment
+python3 -m venv ./venv
+
+# Install pyinstaller
+./venv/bin/pip3 install -r ./requirements.txt
+
+# Package xctest.py
+./venv/bin/pyinstaller ./xctest.py --onedir --clean --distpath ./packaged --workpath ./packaged/build --specpath ./packaged
+
+# Remove pycache
 rm -rf ./__pycache__
